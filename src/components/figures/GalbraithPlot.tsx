3b40022a67754e13778913519a56d16ab0b2641e
jpiccirillo <piccirilloj1@gmail.com>
2025-03-15 21:45:02 -0500

    Update GalbraithPlot.tsx

--- src/components/figures/GalbraithPlot.tsx
+++ src/components/figures/GalbraithPlot.tsx
@@ -1,24 +1,90 @@
-import { Text } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
+import TabLayout from "../layout/TabLayout";
 
 const Component = () => {
   return (
-    <>
-      <Text>
-        <Text fw="600" span>
-          Binary logistic regression
-        </Text>{" "}
-        is used when the outcome variable has only two categories or levels. It
-        models the probability or likelihood of an event occurring based on the
-        predictor variables.
-      </Text>
-
-      <Text>
-        For example, it can be used to predict the likelihood of a patient
-        having a disease (presence/absence) based on their age, gender, and
-        other relevant factors. The outcome variable is encoded as 0 or 1,
-        representing the absence or presence of the event, respectively.
-      </Text>
-    </>
+    <TabLayout
+      description="The Galbraith Plot, also known as the Radial Plot, is a tool used in meta-analysis to assess the consistency of study results. It is particularly useful for identifying outliers and sources of heterogeneity."
+      tabsConfiguration={[
+        {
+          key: "characteristics",
+          label: "Characteristics",
+          content: (
+            <Text>
+              Scatter plot displaying standardized effect sizes (z-scores)
+              against the inverse of their standard errors.
+            </Text>
+          ),
+        },
+        {
+          key: "purpose",
+          label: "Purpose",
+          content: (
+            <Text>
+              To assess heterogeneity among study results and detect potential
+              outliers.
+            </Text>
+          ),
+        },
+        {
+          key: "whenToUse",
+          label: "When to Use",
+          content: (
+            <Text>
+              Useful for evaluating the consistency of study results and
+              identifying heterogeneity in meta-analysis.
+            </Text>
+          ),
+        },
+        {
+          key: "variables",
+          label: "Variables",
+          content: (
+            <Text>
+              <strong>Independent Variable:</strong> Continuous. <br />
+              <strong>Dependent Variable:</strong> Continuous.
+            </Text>
+          ),
+        },
+        {
+          key: "datasetSize",
+          label: "Dataset Size",
+          content: <Text>Medium to Large.</Text>,
+        },
+        {
+          key: "strengths",
+          label: "Strengths",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Outlier Detection:</strong> Effective in identifying
+                outliers and sources of heterogeneity.
+              </Text>
+              <Text>
+                <strong>Visual Clarity:</strong> Provides a clear visual
+                representation of study consistency.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "weaknesses",
+          label: "Weaknesses",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Data Requirement:</strong> Requires a moderate to large
+                dataset for meaningful interpretation.
+              </Text>
+              <Text>
+                <strong>Complexity:</strong> Can be complex to interpret without
+                statistical knowledge.
+              </Text>
+            </Stack>
+          ),
+        },
+      ]}
+    />
   );
 };
 
