8fd669c24654fa151546ad0d1ddb497f7adcf010
jpiccirillo <piccirilloj1@gmail.com>
2025-03-16 00:09:56 -0500

    Update PieChart.tsx

--- src/components/figures/PieChart.tsx
+++ src/components/figures/PieChart.tsx
@@ -1,24 +1,75 @@
-import { Text } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
+import TabLayout from "../layout/TabLayout";
 
 const Component = () => {
   return (
-    <>
-      <Text>
-        <Text fw="600" span>
-          Binary logistic regression
-        </Text>{" "}
-        is used when the outcome variable has only two categories or levels. It
-        models the probability or likelihood of an event occurring based on the
-        predictor variables.
-      </Text>
-
-      <Text>
-        For example, it can be used to predict the likelihood of a patient
-        having a disease (presence/absence) based on their age, gender, and
-        other relevant factors. The outcome variable is encoded as 0 or 1,
-        representing the absence or presence of the event, respectively.
-      </Text>
-    </>
+    <TabLayout
+      description="A Pie Chart visually represents parts of a whole, making it easy to interpret proportions within a group."
+      tabsConfiguration={[
+        {
+          key: "characteristics",
+          label: "Characteristics",
+          content: <Text>Shows parts of a whole, easy to read.</Text>,
+        },
+        {
+          key: "purpose",
+          label: "Purpose",
+          content: <Text>Compare distribution within a group.</Text>,
+        },
+        {
+          key: "whenToUse",
+          label: "When to Use",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Independent Variable:</strong> Categorical
+                (Single-level), Nominal (Single-level), Dichotomous.
+              </Text>
+              <Text>
+                <strong>Dataset Size:</strong> Best for Small datasets due to
+                simplicity.
+              </Text>
+              <Text>
+                <strong>Example:</strong> Proportion of different infection
+                types in a patient population.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "strengths",
+          label: "Strengths",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Easy Interpretation:</strong> Easy to interpret and
+                visually appealing.
+              </Text>
+              <Text>
+                <strong>Proportional Clarity:</strong> Best for showing
+                proportions and parts of a whole.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "weaknesses",
+          label: "Weaknesses",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Dataset Limitations:</strong> Not suitable for large
+                datasets due to simplicity.
+              </Text>
+              <Text>
+                <strong>Comparison Difficulty:</strong> Harder to compare
+                between different groups with too many slices.
+              </Text>
+            </Stack>
+          ),
+        },
+      ]}
+    />
   );
 };
 
