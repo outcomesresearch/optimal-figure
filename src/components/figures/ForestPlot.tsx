d3d2e4627e537c5f44e3bfbf3485b823863f9786
jpiccirillo <piccirilloj1@gmail.com>
2025-03-15 21:38:51 -0500

    Update ForestPlot.tsx

--- src/components/figures/ForestPlot.tsx
+++ src/components/figures/ForestPlot.tsx
@@ -1,24 +1,93 @@
-import { Text } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
+import TabLayout from "../layout/TabLayout";
 
 const Component = () => {
   return (
-    <>
-      <Text>
-        <Text fw="600" span>
-          Binary logistic regression
-        </Text>{" "}
-        is used when the outcome variable has only two categories or levels. It
-        models the probability or likelihood of an event occurring based on the
-        predictor variables.
-      </Text>
-
-      <Text>
-        For example, it can be used to predict the likelihood of a patient
-        having a disease (presence/absence) based on their age, gender, and
-        other relevant factors. The outcome variable is encoded as 0 or 1,
-        representing the absence or presence of the event, respectively.
-      </Text>
-    </>
+    <TabLayout
+      description="The Forest Plot is a key tool in meta-analysis, providing a visual summary of the results from multiple studies. It helps in comparing individual study estimates and the overall pooled estimate."
+      tabsConfiguration={[
+        {
+          key: "characteristics",
+          label: "Characteristics",
+          content: (
+            <Text>
+              Displays estimated results (effect sizes) from multiple studies
+              with confidence intervals.
+            </Text>
+          ),
+        },
+        {
+          key: "purpose",
+          label: "Purpose",
+          content: (
+            <Text>
+              To visually summarize the results of multiple studies in a
+              meta-analysis, showing both the individual study estimates and the
+              overall pooled estimate.
+            </Text>
+          ),
+        },
+        {
+          key: "whenToUse",
+          label: "When to Use",
+          content: (
+            <Text>
+              Ideal for meta-analyses when multiple study estimates need to be
+              synthesized.
+            </Text>
+          ),
+        },
+        {
+          key: "variables",
+          label: "Variables",
+          content: (
+            <Text>
+              <strong>Independent Variable:</strong> Categorical, Continuous,
+              Nominal, Dichotomous. <br />
+              <strong>Dependent Variable:</strong> Continuous.
+            </Text>
+          ),
+        },
+        {
+          key: "datasetSize",
+          label: "Dataset Size",
+          content: <Text>Medium to Large (more than 10 studies).</Text>,
+        },
+        {
+          key: "strengths",
+          label: "Strengths",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Clear Comparison:</strong> Provides a clear visual
+                comparison of individual and pooled study results.
+              </Text>
+              <Text>
+                <strong>Consistency Check:</strong> Useful for identifying the
+                consistency of study findings.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "weaknesses",
+          label: "Weaknesses",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Interpretation Complexity:</strong> Can be difficult to
+                interpret with a large number of studies.
+              </Text>
+              <Text>
+                <strong>Expertise Required:</strong> Requires statistical
+                expertise to understand confidence intervals and pooled
+                estimates.
+              </Text>
+            </Stack>
+          ),
+        },
+      ]}
+    />
   );
 };
 
