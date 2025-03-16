852cc84ab58b03319dfa4ff4cfa3a63d8773529c
jpiccirillo <piccirilloj1@gmail.com>
2025-03-15 21:47:36 -0500

    Update CumulativeMetaAnalysisPlot.tsx

--- src/components/figures/CumulativeMetaAnalysisPlot.tsx
+++ src/components/figures/CumulativeMetaAnalysisPlot.tsx
@@ -1,24 +1,92 @@
-import { Text } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
+import TabLayout from "../layout/TabLayout";
 
 const Component = () => {
   return (
-    <>
-      <Text>
-        <Text fw="600" span>
-          Binary logistic regression
-        </Text>{" "}
-        is used when the outcome variable has only two categories or levels. It
-        models the probability or likelihood of an event occurring based on the
-        predictor variables.
-      </Text>
-
-      <Text>
-        For example, it can be used to predict the likelihood of a patient
-        having a disease (presence/absence) based on their age, gender, and
-        other relevant factors. The outcome variable is encoded as 0 or 1,
-        representing the absence or presence of the event, respectively.
-      </Text>
-    </>
+    <TabLayout
+      description="The Cumulative Meta-Analysis Plot is a technique that sequentially adds studies to show how the cumulative effect size estimate changes over time. It is useful for assessing the stability and trends of the overall effect size."
+      tabsConfiguration={[
+        {
+          key: "characteristics",
+          label: "Characteristics",
+          content: (
+            <Text>
+              Sequentially adds studies to show how the cumulative effect size
+              estimate changes over time.
+            </Text>
+          ),
+        },
+        {
+          key: "purpose",
+          label: "Purpose",
+          content: (
+            <Text>
+              To assess the robustness of the overall effect size and to
+              identify trends or shifts in the effect size as more data becomes
+              available.
+            </Text>
+          ),
+        },
+        {
+          key: "whenToUse",
+          label: "When to Use",
+          content: (
+            <Text>
+              Ideal for evaluating the stability of effect sizes over time and
+              determining when additional studies stop influencing results.
+            </Text>
+          ),
+        },
+        {
+          key: "variables",
+          label: "Variables",
+          content: (
+            <Text>
+              <strong>Independent Variable:</strong> Continuous. <br />
+              <strong>Dependent Variable:</strong> Continuous.
+            </Text>
+          ),
+        },
+        {
+          key: "datasetSize",
+          label: "Dataset Size",
+          content: <Text>Medium to Large (more than 10 studies).</Text>,
+        },
+        {
+          key: "strengths",
+          label: "Strengths",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Trend Analysis:</strong> Provides insights into the
+                stability and trends of the effect size.
+              </Text>
+              <Text>
+                <strong>Effect Size Stability:</strong> Useful for identifying
+                the point at which additional studies no longer significantly
+                change the effect size.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "weaknesses",
+          label: "Weaknesses",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Order Dependency:</strong> Can be influenced by the
+                order in which studies are added.
+              </Text>
+              <Text>
+                <strong>Interpretation Complexity:</strong> Interpretation may
+                be complex without statistical expertise.
+              </Text>
+            </Stack>
+          ),
+        },
+      ]}
+    />
   );
 };
 
