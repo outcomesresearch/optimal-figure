80b54a7fd47c508356cf09e669b98a25714c37f4
jpiccirillo <piccirilloj1@gmail.com>
2025-03-16 00:41:28 -0500

    Update FacetedBarChart.tsx

--- src/components/figures/FacetedBarChart.tsx
+++ src/components/figures/FacetedBarChart.tsx
@@ -1,24 +1,79 @@
-import { Text } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
+import TabLayout from "../layout/TabLayout";
 
 const Component = () => {
   return (
-    <>
-      <Text>
-        <Text fw="600" span>
-          Binary logistic regression
-        </Text>{" "}
-        is used when the outcome variable has only two categories or levels. It
-        models the probability or likelihood of an event occurring based on the
-        predictor variables.
-      </Text>
-
-      <Text>
-        For example, it can be used to predict the likelihood of a patient
-        having a disease (presence/absence) based on their age, gender, and
-        other relevant factors. The outcome variable is encoded as 0 or 1,
-        representing the absence or presence of the event, respectively.
-      </Text>
-    </>
+    <TabLayout
+      description="A Faceted Bar Chart displays multiple bar charts for different subsets of data, making it effective for comparing distributions across more than two groups."
+      tabsConfiguration={[
+        {
+          key: "characteristics",
+          label: "Characteristics",
+          content: (
+            <Text>Multiple bar charts for different subsets of data.</Text>
+          ),
+        },
+        {
+          key: "purpose",
+          label: "Purpose",
+          content: (
+            <Text>Compare distribution across more than two groups.</Text>
+          ),
+        },
+        {
+          key: "whenToUse",
+          label: "When to Use",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Independent Variable:</strong> Categorical (More than
+                Two-level), Nominal (More than Two-level), Dichotomous.
+              </Text>
+              <Text>
+                <strong>Dataset Size:</strong> Suitable for all sizes (Small,
+                Moderate, Large).
+              </Text>
+              <Text>
+                <strong>Example:</strong> Comparing infection rates across
+                different age groups, genders, and hospital departments.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "strengths",
+          label: "Strengths",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Detailed Comparisons:</strong> Provides detailed
+                comparisons across multiple subgroups.
+              </Text>
+              <Text>
+                <strong>Interpretability:</strong> Easy to interpret within
+                individual facets.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "weaknesses",
+          label: "Weaknesses",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Complexity:</strong> Can become complex with too many
+                facets.
+              </Text>
+              <Text>
+                <strong>Organization Challenge:</strong> Requires careful
+                organization for clarity.
+              </Text>
+            </Stack>
+          ),
+        },
+      ]}
+    />
   );
 };
 
