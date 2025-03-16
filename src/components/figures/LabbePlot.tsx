1a7e228228aec499fcdad49fd231921c87db806a
jpiccirillo <piccirilloj1@gmail.com>
2025-03-15 21:43:55 -0500

    Update LabbePlot.tsx

--- src/components/figures/LabbePlot.tsx
+++ src/components/figures/LabbePlot.tsx
@@ -1,24 +1,90 @@
-import { Text } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
+import TabLayout from "../layout/TabLayout";
 
 const Component = () => {
   return (
-    <>
-      <Text>
-        <Text fw="600" span>
-          Binary logistic regression
-        </Text>{" "}
-        is used when the outcome variable has only two categories or levels. It
-        models the probability or likelihood of an event occurring based on the
-        predictor variables.
-      </Text>
-
-      <Text>
-        For example, it can be used to predict the likelihood of a patient
-        having a disease (presence/absence) based on their age, gender, and
-        other relevant factors. The outcome variable is encoded as 0 or 1,
-        representing the absence or presence of the event, respectively.
-      </Text>
-    </>
+    <TabLayout
+      description="The L’Abbe Plot is a graphical method used in meta-analysis to compare the event rates between two groups. It helps in identifying patterns and potential outliers in the data."
+      tabsConfiguration={[
+        {
+          key: "characteristics",
+          label: "Characteristics",
+          content: (
+            <Text>
+              Scatter plot comparing event rates in the experimental group
+              against the control group.
+            </Text>
+          ),
+        },
+        {
+          key: "purpose",
+          label: "Purpose",
+          content: (
+            <Text>
+              To visualize the relationship between the event rates in two
+              groups and assess heterogeneity in meta-analysis studies.
+            </Text>
+          ),
+        },
+        {
+          key: "whenToUse",
+          label: "When to Use",
+          content: (
+            <Text>
+              Suitable for comparing event rates in meta-analysis studies to
+              detect patterns and outliers.
+            </Text>
+          ),
+        },
+        {
+          key: "variables",
+          label: "Variables",
+          content: (
+            <Text>
+              <strong>Independent Variable:</strong> Dichotomous. <br />
+              <strong>Dependent Variable:</strong> Continuous.
+            </Text>
+          ),
+        },
+        {
+          key: "datasetSize",
+          label: "Dataset Size",
+          content: <Text>Any size.</Text>,
+        },
+        {
+          key: "strengths",
+          label: "Strengths",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Comparative Visualization:</strong> Effective in
+                visualizing differences between groups.
+              </Text>
+              <Text>
+                <strong>Heterogeneity Detection:</strong> Useful for detecting
+                heterogeneity and outliers in meta-analysis.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "weaknesses",
+          label: "Weaknesses",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Sample Size Sensitivity:</strong> May be less
+                informative with small sample sizes.
+              </Text>
+              <Text>
+                <strong>Interpretation Difficulty:</strong> Interpretation can
+                be challenging without statistical expertise.
+              </Text>
+            </Stack>
+          ),
+        },
+      ]}
+    />
   );
 };
 
