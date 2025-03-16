581cbc99aedfa5b32da3921be4d789b8686c0803
jpiccirillo <piccirilloj1@gmail.com>
2025-03-16 00:04:07 -0500

    Update GroupedBarChart.tsx

--- src/components/figures/GroupedBarChart.tsx
+++ src/components/figures/GroupedBarChart.tsx
@@ -1,24 +1,76 @@
-import { Text } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
+import TabLayout from "../layout/TabLayout";
 
 const Component = () => {
   return (
-    <>
-      <Text>
-        <Text fw="600" span>
-          Binary logistic regression
-        </Text>{" "}
-        is used when the outcome variable has only two categories or levels. It
-        models the probability or likelihood of an event occurring based on the
-        predictor variables.
-      </Text>
-
-      <Text>
-        For example, it can be used to predict the likelihood of a patient
-        having a disease (presence/absence) based on their age, gender, and
-        other relevant factors. The outcome variable is encoded as 0 or 1,
-        representing the absence or presence of the event, respectively.
-      </Text>
-    </>
+    <TabLayout
+      description="The Grouped Bar Chart is a visualization that compares multiple categories within groups. It is useful for comparing distributions across two groups."
+      tabsConfiguration={[
+        {
+          key: "characteristics",
+          label: "Characteristics",
+          content: <Text>Compares multiple categories within groups.</Text>,
+        },
+        {
+          key: "purpose",
+          label: "Purpose",
+          content: <Text>Compare distribution across two groups.</Text>,
+        },
+        {
+          key: "whenToUse",
+          label: "When to Use",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Independent Variable:</strong> Categorical (Two-level,
+                More than Two-level), Nominal (Two-level, More than Two-level),
+                Dichotomous.
+              </Text>
+              <Text>
+                <strong>Dataset Size:</strong> Suitable for all sizes (Small,
+                Moderate, Large).
+              </Text>
+              <Text>
+                <strong>Example:</strong> Comparing infection rates between male
+                and female patients across different age groups.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "strengths",
+          label: "Strengths",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Comparative Clarity:</strong> Easy to compare multiple
+                groups.
+              </Text>
+              <Text>
+                <strong>Versatility:</strong> Suitable for various dataset
+                sizes.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "weaknesses",
+          label: "Weaknesses",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Cluttered Display:</strong> Can become cluttered with
+                too many categories.
+              </Text>
+              <Text>
+                <strong>Interpretation Complexity:</strong> Harder to interpret
+                relationships between groups.
+              </Text>
+            </Stack>
+          ),
+        },
+      ]}
+    />
   );
 };
 
