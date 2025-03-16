33793e834767c11c17541532e3966946b76abaac
jpiccirillo <piccirilloj1@gmail.com>
2025-03-16 00:58:19 -0500

    Remove dead code from an old project

--- src/assets/continuous/comparison3MoreGroups/ids.ts
+++
@@ -1,9 +0,0 @@
-export const INDEPENDENT =
-  "independent_comparison_three_or_more_groups_continuous";
-export const RELATED = "related_observations_three_or_more_groups_continuous";
-export const ANOVA = "parametric_comparison_three_or_more_groups";
-
-export const REPEATED_MEASURES_ANOVA =
-  "repeated_measures_anova_parametric_assumptions_met";
-export const KRUSKAL_WALLIS = "kruskal_wallis_nonparametric_independent";
-export const FRIEDMANS_TEST = "friedmans_test_nonparametric_related";
--- src/assets/continuous/multiVariableAnalysis/ids.ts
+++
@@ -1,7 +0,0 @@
-export const ARE_OBSEVATIONS_INDEPENDENT =
-  "nonindependent_observations_continuous";
-export const INDEPENDENT_OBSERVATIONS = "observations_are_independent";
-export const LINEAR_REGRESSION = "linear_regression_continuous";
-export const POISSON_REGRESSION = "poisson_regression_continuous";
-export const ORDINAL_REGRESSION = "orginal_regression_continuous";
-export const MIXED_LINEAR_MODEL = "mixed_linear_model";
