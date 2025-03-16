bd8745963122b87aca9e5a3417dbf0fd9090273c
jpiccirillo <piccirilloj1@gmail.com>
2025-03-15 21:41:44 -0500

    Update FunnelPlot.tsx

--- src/components/figures/FunnelPlot.tsx
+++ src/components/figures/FunnelPlot.tsx
@@ -1,24 +1,90 @@
-import { Text } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
+import TabLayout from "../layout/TabLayout";
 
 const Component = () => {
   return (
-    <>
-      <Text>
-        <Text fw="600" span>
-          Binary logistic regression
-        </Text>{" "}
-        is used when the outcome variable has only two categories or levels. It
-        models the probability or likelihood of an event occurring based on the
-        predictor variables.
-      </Text>
-
-      <Text>
-        For example, it can be used to predict the likelihood of a patient
-        having a disease (presence/absence) based on their age, gender, and
-        other relevant factors. The outcome variable is encoded as 0 or 1,
-        representing the absence or presence of the event, respectively.
-      </Text>
-    </>
+    <TabLayout
+      description="The Funnel Plot is used in meta-analysis to detect publication bias by visualizing the relationship between study size and effect size. It helps in identifying asymmetry that may indicate bias."
+      tabsConfiguration={[
+        {
+          key: "characteristics",
+          label: "Characteristics",
+          content: (
+            <Text>
+              Scatter plot showing the relationship between study size and
+              effect size, resembling an inverted funnel.
+            </Text>
+          ),
+        },
+        {
+          key: "purpose",
+          label: "Purpose",
+          content: (
+            <Text>
+              To assess publication bias in meta-analyses by visualizing the
+              distribution of study results.
+            </Text>
+          ),
+        },
+        {
+          key: "whenToUse",
+          label: "When to Use",
+          content: (
+            <Text>
+              Ideal for evaluating the presence of publication bias in
+              meta-analyses.
+            </Text>
+          ),
+        },
+        {
+          key: "variables",
+          label: "Variables",
+          content: (
+            <Text>
+              <strong>Independent Variable:</strong> Continuous. <br />
+              <strong>Dependent Variable:</strong> Continuous.
+            </Text>
+          ),
+        },
+        {
+          key: "datasetSize",
+          label: "Dataset Size",
+          content: <Text>Medium to Large (more than 10 studies).</Text>,
+        },
+        {
+          key: "strengths",
+          label: "Strengths",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Bias Detection:</strong> Effective in detecting
+                publication bias.
+              </Text>
+              <Text>
+                <strong>Visual Insight:</strong> Provides a visual
+                representation of study distribution and potential asymmetry.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "weaknesses",
+          label: "Weaknesses",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Subjectivity:</strong> Interpretation can be subjective
+                and influenced by the number of studies.
+              </Text>
+              <Text>
+                <strong>Limited Effectiveness:</strong> Less effective with a
+                small number of studies.
+              </Text>
+            </Stack>
+          ),
+        },
+      ]}
+    />
   );
 };
 
