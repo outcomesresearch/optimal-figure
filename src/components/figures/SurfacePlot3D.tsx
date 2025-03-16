cfa97c07a65e43f6f571c9fa8f385ff61a0f4a7a
jpiccirillo <piccirilloj1@gmail.com>
2025-03-16 00:19:09 -0500

    Delete SurfacePlot3D.tsx

--- src/components/figures/SurfacePlot3D.tsx
+++ src/components/figures/SurfacePlot3D.tsx
@@ -1,24 +1,79 @@
-import { Text } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
+import TabLayout from "../layout/TabLayout";
 
 const Component = () => {
   return (
-    <>
-      <Text>
-        <Text fw="600" span>
-          Binary logistic regression
-        </Text>{" "}
-        is used when the outcome variable has only two categories or levels. It
-        models the probability or likelihood of an event occurring based on the
-        predictor variables.
-      </Text>
-
-      <Text>
-        For example, it can be used to predict the likelihood of a patient
-        having a disease (presence/absence) based on their age, gender, and
-        other relevant factors. The outcome variable is encoded as 0 or 1,
-        representing the absence or presence of the event, respectively.
-      </Text>
-    </>
+    <TabLayout
+      description="A 3D Surface Plot visualizes relationships between three continuous variables, providing a detailed three-dimensional representation of data."
+      tabsConfiguration={[
+        {
+          key: "characteristics",
+          label: "Characteristics",
+          content: (
+            <Text>
+              Visualizes relationships between three continuous variables.
+            </Text>
+          ),
+        },
+        {
+          key: "purpose",
+          label: "Purpose",
+          content: <Text>Show relationship between variables.</Text>,
+        },
+        {
+          key: "whenToUse",
+          label: "When to Use",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Independent Variable:</strong> Continuous (Single-level,
+                Two-level, More than Two-level).
+              </Text>
+              <Text>
+                <strong>Dataset Size:</strong> Suitable for Moderate to Large
+                datasets.
+              </Text>
+              <Text>
+                <strong>Example:</strong> Relationship between age, BMI, and
+                cholesterol levels.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "strengths",
+          label: "Strengths",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Detailed Visualization:</strong> Provides a detailed
+                three-dimensional view of data.
+              </Text>
+              <Text>
+                <strong>Complex Relationship Analysis:</strong> Effective for
+                visualizing complex relationships.
+              </Text>
+            </Stack>
+          ),
+        },
+        {
+          key: "weaknesses",
+          label: "Weaknesses",
+          content: (
+            <Stack>
+              <Text>
+                <strong>Interpretation Difficulty:</strong> Can be difficult to
+                interpret without interactive tools.
+              </Text>
+              <Text>
+                <strong>Obscured Details:</strong> Overlapping surfaces may
+                obscure details.
+              </Text>
+            </Stack>
+          ),
+        },
+      ]}
+    />
   );
 };
 
