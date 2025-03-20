60af330f96aa2c6fb2117945f693a194a6ada832
jpiccirillo <piccirilloj1@gmail.com>
2025-03-19 23:15:40 -0500

    Update ids.ts

--- src/assets/categorical/ids.ts
+++ src/assets/categorical/ids.ts
@@ -1,16 +1,16 @@
-export const INDEPENDENT_TYPE = "independent_type";
-export const INDEPENDENT_CAT = "independent_cat";
-export const INDEPENDENT_CONT = "independent_cont";
+export const INDEPENDENT_TYPE = "independent-type";
+export const INDEPENDENT_CAT = "independent-cat";
+export const INDEPENDENT_CONT = "independent-cont";
 
-export const DS_CAT = "ds_cat";
-export const DS_CONT = "ds_cont";
+export const DS_CAT = "ds-cat";
+export const DS_CONT = "ds-cont";
 
-export const FIG_ICON_ARRAY = "fig_icon_array";
-export const FIG_VERTICAL_BAR = "fig_vertical_bar";
-export const FIG_STACKED_BAR = "fig_stacked_bar";
-export const FIG_HORIZONTAL_BAR = "fig_horizontal_bar";
-export const FIG_MOSAIC_PLOT = "fig_mosaic_plot";
+export const FIG_ICON_ARRAY = "fig-icon-array";
+export const FIG_VERTICAL_BAR = "fig-vertical-bar";
+export const FIG_STACKED_BAR = "fig-stacked-bar";
+export const FIG_HORIZONTAL_BAR = "fig-horizontal-bar";
+export const FIG_MOSAIC_PLOT = "fig-mosaic-plot";
 
-export const CHOOSE_SMALL_CAT = "choose_small_category"
-export const CHOOSE_MEDIUM_CAT = "choose_medium_category"
-export const CHOOSE_LARGE_CAT = "choose_large_category"
+export const CHOOSE_SMALL_CAT = "choose-small-category";
+export const CHOOSE_MEDIUM_CAT = "choose-medium-category";
+export const CHOOSE_LARGE_CAT = "choose-large-category";
